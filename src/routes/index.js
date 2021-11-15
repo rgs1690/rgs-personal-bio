a60de94ab4f33b88abf989191b270a7029923efc
Grace Sutherland <rgs1690@gmail.com>
2021-11-15 07:46:14 -0600

    added file structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,10 @@
-// index for router
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" />
+    </Switch>
+  );
+}
