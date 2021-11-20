4a4e2073e923ad284d54ee33e7b86e356a4ea995
Grace Sutherland <rgs1690@gmail.com>
2021-11-20 10:51:50 -0600

    styling, changed if else statement in initalize to null instead of false to fix error

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,55 +1,69 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import { signOutUser, signInUser } from '../api/auth';
 
-// const NavStyle = styled.div`
-// Nav {
-//   display: flex;
-//   flex-flow: row wrap;
-//   justify-content: right;
-// }
-
-// `;
+const NavStyle = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: left;
+  .navItem {
+    color: white;
+    font-size: 2em;
+    font-style: bold;
+  }
+  .btn {
+    background-color: transparent;
+    border: 0;
+    color: transparent;
+  }
+`;
 export default function Navigation({ user }) {
   return (
     <>
-      <p className="text-center mt-4 mb-4" />
-      <Nav className="justify-content-end" activeKey="/home">
-        <Nav.Item>
-          <Nav.Link href="/">Home</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/about">About</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/projects">Projects</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/tech">Tech</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/contact">Contact Me</Nav.Link>
-        </Nav.Item>
-        {user ? (
-          <button
-            onClick={signOutUser}
-            type="button"
-            className="btn btn-primary"
-          >
-            Admin SignOut
-          </button>
-        ) : (
-          <button
-            onClick={signInUser}
-            type="button"
-            className="btn btn-primary"
-          >
-            Admin SignIn
-          </button>
-        )}
-      </Nav>
+      <NavStyle>
+        <Nav className="justify-content-end" activeKey="/home">
+          <Nav.Item>
+            <Nav.Link className="navItem" href="/">
+              Home
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="navItem" href="/about">
+              About
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="navItem" href="/projects">
+              Projects
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="navItem" href="/tech">
+              Tech
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="navItem" href="/contact">
+              Contact Me
+            </Nav.Link>
+          </Nav.Item>
+          {user ? (
+            <button
+              onClick={signOutUser}
+              type="button"
+              className="btn btn-primary"
+            >
+              Admin SignOut
+            </button>
+          ) : (
+            <button onClick={signInUser} type="button" className="btn">
+              Admin SignIn
+            </button>
+          )}
+        </Nav>
+      </NavStyle>
     </>
   );
 }
